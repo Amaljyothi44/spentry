943ff491288c185437885269aca987b87b5b1066
amaljyothi44 <amaljyothi414@gmail.com>
2023-06-17 18:06:57 +0530

    third

--- src/App.js
+++ src/App.js
@@ -247,7 +247,7 @@ axios.defaults.xsrfHeaderName = 'X-CSRFToken';
 axios.defaults.withCredentials = true;
 
 const client = axios.create({
-  baseURL: "http://127.0.0.1:8000"
+  baseURL: "http://amaljyothi444.pythonanywhere.com/"
 });
 
 function App() {
